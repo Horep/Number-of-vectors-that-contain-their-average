6a7c0a82de6663d7ba3cf3fda0779526bd419cca
Horep <horep245@gmail.com>
2020-08-27 00:19:00 +0100

    Rewritten specifically for mentioned sequences

---
+++ A3377110.py
@@ -0,0 +1,26 @@
+import itertools
+import numpy as np
+
+
+def geo_pre_mean(iterable):  # Returns product of members of list
+    a = np.array(iterable)
+    return a.prod()
+
+
+def geo_avg_in_set(x):  # Checks if the pre mean is in the list
+    N = len(x)
+    x_1 = [y**N for y in x]
+    if x_1.count(geo_pre_mean(x)) > 0:
+        return True
+    else:
+        return False
+
+
+def a(n):  # Calculates exact number of vectors that contain their geo mean
+    prob = 0
+    for i in itertools.product(range(1, 3+1), repeat=n):
+        x = list(i)
+        Add = geo_avg_in_set(x)
+        if Add is True:
+            prob += 1
+    return prob
