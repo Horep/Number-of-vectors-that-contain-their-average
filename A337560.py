51b8d91284b15f3f924dbdb4b7930fc9964faa3c
Horep <horep245@gmail.com>
2020-09-02 02:20:44 +0100

    Improved efficiency.

--- A337560.py
+++ A337560.py
@@ -1,6 +1,18 @@
 import itertools
 
 
+def num_of_perms(v):
+    mysetlen = len(set(v))
+    if mysetlen == 4:
+        return 24
+    elif mysetlen == 3:
+        return 12
+    elif mysetlen == 2:
+        return 6
+    elif mysetlen == 1:
+        return 1
+
+
 def H(v):
     summand = v[1]*v[2]*v[3] + v[0]*v[2]*v[3] + v[0]*v[1]*v[3] + v[0]*v[1]*v[2]
     product = 4*v[0]*v[1]*v[2]*v[3]
@@ -15,11 +27,11 @@ def a(n):  # Calculates exact number of vectors that contain their geo mean
         prob = 1
     else:  # Brute force method
         prob = 0
-        for i in itertools.product(range(1, n+1), repeat=4):
+        for i in itertools.combinations_with_replacement(range(1, n+1), 4):
             x = list(i)
             Add = H(x)
             if Add is True:
-                prob += 1
+                prob += num_of_perms(x)
     return prob
 
 
